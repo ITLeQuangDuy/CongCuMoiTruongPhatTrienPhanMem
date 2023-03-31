ccd085ebb9f8472e67ac3feeb4472befcc038858
LeQuangDuy <it.lequangduy@gmail.com>
2023-03-31 15:59:37 +0700

    tai len web TMDT

---
+++ add_product.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once("./entities/product.class.php");
+require_once("./entities/category.class.php");
+
+if(isset($_POST["btnsubmit"])){
+    $productName = $_POST["txtName"];
+    $cateID = $_POST["txtCateID"];
+    $price = $_POST["txtprice"];
+    $quantity = $_POST["txtquantity"];
+    $description = $_POST["txtdesc"];
+    $picture = $_POST["txtpic"];
+
+    $newProduct = new Product($productName, $cateID, $price, $quantity, $desription, $picture);
+
+    $result = $newProduct->save();
+    if(!$result)
+    {
+        header("Location: add_product.php?failure");
+    }
+    else {
+        header("Location: add_product.php?inserted");
+    }
+}
+?>
+<?php include_once("header.php"); ?>
+
+<?php
+    if(isset($_GET["inserted"])){
+        echo "<h2>Thêm sản phẩm thành công</h2>";   
+    }
+    ?>
+<!-- #form sản phẩm -->
+<form method="post">
+    <!-- #Tên SP -->
+    <div class="row">
+        <div class="lbltitle">
+            <label>Tên sản phẩm</label>
+        </div>
+        <div class="lblinput">
+            <input type="text" name="txtName" value="<?php echo isset($_POST["txtName"]) ? $_POST["txtName"] : "" ; ?>" />
+        </div>
+    </div>
+    <!-- #mô tả sản phẩm -->
+    <div class="row">
+        <div class="lbltitle">
+            <label>Mô tả sản phẩm</label>
+        </div>
+        <div class="lblinput">
+            <textarea name="txtdesc" cols="21" rows="10" value="<?php echo isset($_POST["txtdesc"]) ? $_POST["txtdesc"] : "" ; ?>"></textarea>
+        </div>
+    </div>
+    <!-- #số lượng sản phẩm -->
+    <div class="row">
+    <!-- #giá sản phẩm -->
+    <div class="row">
+    <!-- #loại sản phẩm -->
+    <div class="row">
+        <div class="lbltitle">
+            <label>Chọn loại sản phẩm</label>
+        </div>
+        <div class="lblinput">
+            <select name="txtCateID">
+                <option value="" selected>--Chọn Loại--</option>
+                <?php
+                    $cates = Category::list_category();
+                    foreach ($cates as $item) {
+                        echo "<option value=".$item["CateID"].">".$item["CategoryName"]."</option>";
+                    }
+                    ?>
+            </select>
+        </div>
+    </div>
+    <!-- #hình ảnh -->
+    <div class="row">
+        <div class="lbltitle">
+            <label>Đường dẫn hình</label>
+        </div>
+        <div class="lblinput">
+            <input type="file" id="txtpic" name="txtpic" accept=".PNG,.GIF,.JPG">
+        </div>
+    </div>
+    <!-- #nút gửi form -->
+    <div class="row">
+        <div class="submit">
+            <input type="submit" name="btnsubmit" value="Thêm sản phẩm" />
+        </div>
+    </div>
+</form>
---
+++ entities/product.class.php
@@ -0,0 +1,62 @@
+<?php 
+require_once("/config/db.class.php");
+
+class Product
+{
+    public $productID;
+    public $productName;
+    public $cateID;
+    public $price;
+    public $quantity;
+    public $description;
+    public $picture;
+
+    public function __construct($pro_name, $cate_id, $price, $quantity, $desc, $picture)
+    {
+        $this->productName = $pro_name;
+        $this->cateID = $cate_id;
+        $this->price = $price;
+        $this->$quantity = $quantity;
+        $this->description = $desc;
+        $this->picture = $picture;
+    }
+
+    public function save(){
+
+        //xử lí upload hình ảnh
+        $file_temp = $this->picture['tmp_name'];
+        $user_file = $this->picture['name'];
+        $timestamp = date("Y")
+
+        //end upload file
+        $db = new Db();
+
+        $sql = "INSERT INTO Product(ProductName, CateID, Price, Quantity, Desription, Picture) VALUES
+        ('$this->productName','$this->cateID','$this->price','$this->quantity','$this->description','$this->picture')";
+
+        $result = $db->query_execute($sql);
+        return $result;
+    }
+
+    public static function list_product(){
+        $db = new Db();
+        $sql = "SELECT * FROM product";
+        $result = $db->select_to_array($sql);
+        return $result;
+    }
+
+    public static function list_product_relate(){
+        $db = new Db();
+        $sql = "SELECT * FROM product WHERE CateID='$cateid' AND productID!=$id'";
+        $result = $db->select_to_array($sql);
+        return $result;
+    }
+
+    public static function get_product($id){
+        $db = new Db();
+        $sql = "SELECT * FROM product WHERE productID= '$id'";
+        $result = $db->select_to_array($sql);
+        return $result;
+    }
+}
+?>
